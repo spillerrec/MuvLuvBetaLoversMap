7d8473054424b27d244ff0a6142247f5c799bb2e
spillerrec <spillerrec@hotmail.com>
2015-11-01 17:09:52 +0100

    ADD: a bit of styling

--- index.php
+++ index.php
@@ -6,8 +6,23 @@
   <script src="jquery-1.11.3.min.js"></script>
   <script src="jvectormap/jquery-jvectormap-2.0.3.min.js"></script>
   <script src="jvectormap/jquery-jvectormap-world-mill.js"></script>
+  <link href='https://fonts.googleapis.com/css?family=Share+Tech+Mono' rel='stylesheet' type='text/css'>
   <style>
-		html, body, #world-map{ margin:0; width: 100%; height: 100% }
+		html, body, #world-map{
+				margin:0;
+				width: 100%;
+				height: 100%;
+				font-family: 'Share Tech Mono', monospace;
+				color: yellow;
+				font-weight: bold;
+				cursor: url("crosshair.png") 16 16, crosshair;
+			}
+		.jvectormap-tip{
+			font-family: 'Share Tech Mono', monospace;
+			color: yellow;
+			font-weight: bold;
+			border: 2px solid yellow;
+		}
   </style>
 </head>
 <body>
@@ -28,10 +43,23 @@
 		$(function(){
 			$('#world-map').vectorMap(
 				{	map: 'world_mill'
+				,	backgroundColor: "#000026"
+				,	regionStyle:
+					{	initial:
+						{ 	fill: "#131969"
+						,	stroke: "#42478d"
+						,	"stroke-width": "1px"
+						}
+					,	hover: { cursor: "url('crosshair.png') 16 16" }
+					}
+				,	regionLabelStyle:
+					{	hover:
+						{	"font-family": "Share Tech Mono" }
+					}
 				,	series: {
 						regions:
 							[{	values: gdpData
-							,	scale: ['#C8EEFF', '#0071A4']
+							,	scale: ['#480270', '#900266']
 							,	normalizeFunction: 'polynomial'
 							}]
 					}
